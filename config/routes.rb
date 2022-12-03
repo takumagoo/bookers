5ec3623b53fbf5f55e180f2a8685fa62aa7b1206
takumagoo <117556435+takumagoo@users.noreply.github.com>
2022-12-03 02:37:14 +0000

    [add] 3-課題2 form_with記入

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
-  get 'news/index'
-  post 'news'=>'news#create'
-  get 'new/show'
-  get 'new/edit'
+  get 'books/new'
+  post 'books'=>'books#create'
+  get 'books/index'
+  get 'books/show'
+  get 'books/edit'
+  # get 'news/index'
+  # post 'news'=>'news#create'
+  # get 'new/show'
+  # get 'new/edit'
   get 'homes/top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

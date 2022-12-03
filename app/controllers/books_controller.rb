99f7f07dced8534b948844513a4489a983ba651d
takumagoo <117556435+takumagoo@users.noreply.github.com>
2022-12-03 06:34:54 +0000

    [add] 3-課題2 indexページ作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,7 +3,14 @@ class BooksController < ApplicationController
   end
 
   def index
-    @books = Book.new
+    @book = Book.new
+    @books = Book.all
+  end
+
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to '/books/show'
   end
 
   def show
@@ -11,4 +18,11 @@ class BooksController < ApplicationController
 
   def edit
   end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
 end

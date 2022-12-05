9aca7635b772ea5e9726d05a64dd53021ee5dd53
takumagoo <117556435+takumagoo@users.noreply.github.com>
2022-12-05 06:45:47 +0000

    [add] 3-課題2 フラッシュメッセージ 完

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -10,7 +10,9 @@ class BooksController < ApplicationController
   def create
     book = Book.new(book_params)
     book.save
-    redirect_to '/books/show'
+    flash[:notice] = "投稿が成功しました"
+    # redirect_to '/books/show'
+    redirect_to book_path(book.id)
   end
 
   def show
@@ -19,6 +21,19 @@ class BooksController < ApplicationController
 
   def edit
     @book = Book.find(params[:id])
+  end
+
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    flash[:notice] = "投稿が成功しました"
+    redirect_to book_path(book.id)
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
   end
 
   private
